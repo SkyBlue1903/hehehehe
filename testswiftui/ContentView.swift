7efaf275fb3a12d1f4f3e69edae241a8d59ff599
Erlangga Anugrah Arifin <angga1433@gmail.com>
2026-05-22 10:33:24 +0700

    commit 1 vio

--- testswiftui/ContentView.swift
+++ testswiftui/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
       Image(systemName: "person.fill")
         .imageScale(.large)
         .foregroundStyle(.tint)
-      Text("Hello, Angga!")
+      Text("Hello, Vio!")
     }
     .padding()
   }
