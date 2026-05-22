7146c9bcb6ebf5c76048ca61a9069010471b3077
Erlangga Anugrah Arifin <angga1433@gmail.com>
2026-05-22 10:24:25 +0700

    commit 3

--- testswiftui/ContentView.swift
+++ testswiftui/ContentView.swift
@@ -8,17 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, Angga!")
-        }
-        .padding()
+  var body: some View {
+    VStack {
+      Image(systemName: "person.fill")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, Angga!")
     }
+    .padding()
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
