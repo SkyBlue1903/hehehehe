d6b08971b4705861936d2e926012a7654fe62e92
Erlangga Anugrah Arifin <angga1433@gmail.com>
2026-05-22 10:16:13 +0700

    commit pertama

---
+++ testswiftui/testswiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  testswiftuiApp.swift
+//  testswiftui
+//
+//  Created by Erlangga Anugrah Arifin on 22/05/26.
+//
+
+import SwiftUI
+
+@main
+struct testswiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
