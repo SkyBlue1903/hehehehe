4b910773a29ca90e87fac6d2e8a80cb4b1da8c63
Erlangga Anugrah Arifin <angga1433@gmail.com>
2026-05-26 10:15:34 +0700

    justin commit 2

--- testswiftui/ContentView.swift
+++ testswiftui/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
         .foregroundStyle(.tint)
       Text("Hello, Justin!")
         .font(.largeTitle)
+      DatePicker(selection: /*@START_MENU_TOKEN@*/.constant(Date())/*@END_MENU_TOKEN@*/, label: { /*@START_MENU_TOKEN@*/Text("Date")/*@END_MENU_TOKEN@*/ })
     }
     .padding()
   }
